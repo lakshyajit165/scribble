9fad1ec3eb48da1cb755a63da950fbd04c651ba4
Lakshyajit Laxmikant <lakshyajit165@gmail.com>
2022-11-19 09:16:10 +0530

    added auto login after signup in ui

--- scribble-app/src/app/components/signup/signup.component.ts
+++ scribble-app/src/app/components/signup/signup.component.ts
@@ -3,8 +3,10 @@ import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, FormControl, AbstractControl, FormGroupDirective, NgForm, Validators} from '@angular/forms';
 import {ErrorStateMatcher} from '@angular/material/core';
 import { MatStepper } from '@angular/material/stepper';
+import { Router } from '@angular/router';
 import { IConfirmPassword } from 'src/app/model/IConfirmPassword';
 import { IGenericResponse } from 'src/app/model/IGenericResponse';
+import { ILogin } from 'src/app/model/ILogin';
 import { ISignUpAndForgotPassword } from 'src/app/model/ISignUpAndForgotPassword';
 import { AuthService } from 'src/app/services/auth/auth.service';
 import { SnackbarService } from 'src/app/utils/snackbar.service';
@@ -27,7 +29,8 @@ export class SignupComponent implements OnInit {
   constructor(
     private _formBuilder: FormBuilder,
     private _authService: AuthService,
-    private _snackBarService: SnackbarService
+    private _snackBarService: SnackbarService,
+    private _router: Router
   ) { }
 
   ngOnInit(): void {
@@ -52,23 +55,27 @@ export class SignupComponent implements OnInit {
     password: '',
     verification_code: ''
   };
-  signUpAndForgotPasswordLoading: boolean = false;
+  userLoginPayload: ILogin = {
+    email: '',
+    password: ''
+  };
+  signUpLoading: boolean = false;
   confirmPasswordLoading: boolean = false;
 
   signUpUser(stepper: MatStepper): void {
     // make loading to true
-    this.signUpAndForgotPasswordLoading = true;
+    this.signUpLoading = true;
     // get the values from both forms
     if(this.emailFormGroup.valid){
       this.signUpAndForgotPasswordPayload.email = this.emailFormGroup.get('email')?.value ?? '';
       this._authService.signUp(this.signUpAndForgotPasswordPayload).subscribe({
         next: (data: IGenericResponse) => {
-          this.signUpAndForgotPasswordLoading = false;
+          this.signUpLoading = false;
           this._snackBarService.showSnackBar(data.message, 3000, 'check_circle_outline');
           stepper.next();
         },
         error: err => {
-          this.signUpAndForgotPasswordLoading = false;
+          this.signUpLoading = false;
           if(err.error && err.error.message) {
             this._snackBarService.showSnackBar(err.error.message, 3000, 'error_outline');
           }else{
@@ -77,7 +84,7 @@ export class SignupComponent implements OnInit {
         } 
       })
     }else{
-      this.signUpAndForgotPasswordLoading = false;
+      this.signUpLoading = false;
       this._snackBarService.showSnackBar("Invalid data!", 3000, 'error_outline');
     }
     
@@ -92,9 +99,14 @@ export class SignupComponent implements OnInit {
       this.confirmPasswordPayload.verification_code = this.confirmPasswordFormGroup.get('verificationCode')?.value ?? '';
       this._authService.confirmPassword(this.confirmPasswordPayload).subscribe({
         next: (data: IGenericResponse) => {
-          this.signUpAndForgotPasswordLoading = false;
-          this._snackBarService.showSnackBar(data.message || 'SignUp succesful!', 3000, 'check_circle_outline');
-          // automatically login user here.
+          this.confirmPasswordLoading = false;
+          this._snackBarService.showSnackBar(data.message || 'SignUp successful, logging in!', 3000, 'check_circle_outline');
+          /**
+           * 1. call the login api
+           * 2. Login success => route to dashboard and show appropriate msg
+           * 3. Login fail => route to login page and show appropriate msg
+           */
+          this.login(this.confirmPasswordPayload.email, this.confirmPasswordPayload.password);
         },
         error: err => {
           this.confirmPasswordLoading = false;
@@ -111,6 +123,31 @@ export class SignupComponent implements OnInit {
     }
   }
 
- 
+  login(email: string, password: string): void {
+    this.confirmPasswordLoading = true;
+    if(email && password){
+      this.userLoginPayload.email = email;
+      this.userLoginPayload.password = password;
+      this._authService.login(this.userLoginPayload).subscribe({
+        next: (data: IGenericResponse) => {
+          this.confirmPasswordLoading = false;
+          this._router.navigate(['/home']);
+        },
+        error: err => {
+          this.confirmPasswordLoading = false;
+          if(err.error && err.error.message) {
+            this._snackBarService.showSnackBar(err.error.message, 3000, 'error_outline');
+          }else{
+            this._snackBarService.showSnackBar("An error occurred. Please try again!", 3000, 'error_outline');
+          }
+          this._router.navigate(['/login']);
+        } 
+      })
+    }else{
+      this.confirmPasswordLoading = false;
+      this._snackBarService.showSnackBar("Invalid data!", 3000, 'error_outline');
+      this._router.navigate(['/login']);
+    }
+  }
 
 }
